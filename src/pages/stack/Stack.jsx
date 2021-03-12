b288c58fe5f490b45c549fd51edbb57fa8f99acb
paulords95 <paulords95@gmail.com>
2021-03-12 10:47:05 -0300

    added fade in effect

--- src/pages/stack/Stack.jsx
+++ src/pages/stack/Stack.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./stack.css";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 //svgs
 import { ReactComponent as JavaScript } from "../../assets/svgs/javascript.svg";
@@ -11,6 +14,10 @@ import { ReactComponent as PostgreSQL } from "../../assets/svgs/postgresql.svg";
 import { ReactComponent as Css3 } from "../../assets/svgs/css3.svg";
 
 const StackPage = () => {
+  useEffect(() => {
+    Aos.init({ duration: 3000 });
+  }, []);
+
   return (
     <div className="stack-page-container">
       <div className="title-stack-wrap">
@@ -18,27 +25,51 @@ const StackPage = () => {
         <hr />
       </div>
       <div className="stack-items-wrap">
-        <div className="js-item-container">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="react-item-container"
+        >
           <JavaScript />
           <h3>JavaScript</h3>
         </div>
-        <div className="react-item-container">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="react-item-container"
+        >
           <ReactLogo />
           <h3>React</h3>
         </div>
-        <div className="react-native-item-container">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="react-native-item-container"
+        >
           <ReactNative />
           <h3>React Native</h3>
         </div>
-        <div className="nodejs-item-container">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="nodejs-item-container"
+        >
           <NodeJs />
           <h3>Node.js</h3>
         </div>
-        <div className="postgresql-item-container">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="postgresql-item-container"
+        >
           <PostgreSQL />
           <h3>PostgreSQL</h3>
         </div>
-        <div className="css3-item-container">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="css3-item-container"
+        >
           <Css3 />
           <h3>CSS3</h3>
         </div>
