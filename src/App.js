95992700c17cc069319285d8fedb6dfb74e2bb28
paulords95 <paulords95@gmail.com>
2021-03-12 08:42:42 -0300

    created stack page and title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
 
-import Home from './pages/home/Home'
+import Home from "./pages/home/Home";
+import StackPage from "./pages/stack/Stack";
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Home />
+      <StackPage />
     </div>
   );
 }
---
+++ src/pages/stack/Stack.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./stack.css";
+
+const StackPage = () => {
+  return (
+    <div className="stack-page-container">
+      <div className="title-stack-wrap">
+        <h1 className="title-stack">Meu Stack</h1>
+        <hr />
+      </div>
+      <div className="stack-items-wrap"></div>
+    </div>
+  );
+};
+
+export default StackPage;
