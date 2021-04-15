da43d6da5c7766f30182eab29b079aa3bce7a66b
paulords95 <paulords95@gmail.com>
2021-04-15 09:34:46 -0300

    added carrousel

--- src/components/CardColumn/CardColumn.jsx
+++ src/components/CardColumn/CardColumn.jsx
@@ -1,20 +1,49 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import { CardMedia } from "@material-ui/core";
+import Carousel from "nuka-carousel";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 import "./CardColumn.css";
 
 const CardColumn = () => {
   return (
     <div className="column-card-container">
-      <div className="column-info"></div>
+      <div className="column-info">
+        <h3>Dax App - React Native, Node.js e PostgreSQL</h3>
+        <p>
+          Monitora preço e regitra em banco dados PostgreSQL, faz consultas na
+          API da carteira da NovaDAX e calcula porcentagem e valor de patrimônio
+        </p>
+      </div>
       <div className="column-screen">
-        <Card className="card-wrap">
-          <CardMedia
-            className="media"
-            image="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150539_Dax%20API.jpg"
+        <Carousel
+          wrapAround
+          defaultControlsConfig={{
+            nextButtonText: <ArrowForwardIosIcon />,
+            prevButtonText: <ArrowBackIosIcon />,
+            pagingDotsStyle: {
+              fill: "grey",
+            },
+          }}
+        >
+          <img
+            alt="1"
+            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150530_Dax%20API.jpg"
           />
-        </Card>
+
+          <img
+            alt="2"
+            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150539_Dax%20API.jpg"
+          />
+          <img
+            alt="3"
+            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150547_Dax%20API.jpg"
+          />
+          <img
+            alt="4"
+            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150551_Dax%20API.jpg"
+          />
+        </Carousel>
       </div>
     </div>
   );
--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import { CardMedia } from "@material-ui/core";
 
 import "./ProjectCard.css";
 
@@ -8,15 +6,7 @@ const ProjectCard = () => {
   return (
     <div className="project-card-container">
       <div className="project-info"></div>
-      <div className="project-screen">
-        <Card>
-          <CardMedia
-            className="media"
-            image="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150539_Dax%20API.jpg"
-          />
-        </Card>
-        aaa
-      </div>
+      <div className="project-screen"></div>
     </div>
   );
 };
