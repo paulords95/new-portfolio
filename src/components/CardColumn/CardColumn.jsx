2d3a0f48ca9d18334867f7bda1d65d4bd24964d3
paulords95 <paulords95@gmail.com>
2021-04-16 10:18:33 -0300

    styled item

--- src/components/CardColumn/CardColumn.jsx
+++ src/components/CardColumn/CardColumn.jsx
@@ -7,36 +7,44 @@ import "./CardColumn.css";
 
 const CardColumn = () => {
   return (
-    <div className="column-card-container">
-      <div className="column-screen">
-        <Carousel
-          wrapAround
-          defaultControlsConfig={{
-            nextButtonText: <ArrowForwardIosIcon />,
-            prevButtonText: <ArrowBackIosIcon />,
-            pagingDotsStyle: {
-              fill: "grey",
-            },
-          }}
-        >
-          <img
-            alt="1"
-            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150530_Dax%20API.jpg"
-          />
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexWrap: "wrap-reverse",
+      }}
+    >
+      <div className="column-card-container">
+        <div className="column-screen">
+          <Carousel
+            wrapAround
+            defaultControlsConfig={{
+              nextButtonText: <ArrowForwardIosIcon />,
+              prevButtonText: <ArrowBackIosIcon />,
+              pagingDotsStyle: {
+                fill: "grey",
+              },
+            }}
+          >
+            <img
+              alt="1"
+              src="https://raw.githubusercontent.com/paulords95/novadax-app/main/Screenshot_20210409-150530_Dax%20API.jpg"
+            />
 
-          <img
-            alt="2"
-            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150539_Dax%20API.jpg"
-          />
-          <img
-            alt="3"
-            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150547_Dax%20API.jpg"
-          />
-          <img
-            alt="4"
-            src="https://raw.githubusercontent.com/paulords95/novadax-app/main/screenshots/Screenshot_20210409-150551_Dax%20API.jpg"
-          />
-        </Carousel>
+            <img
+              alt="2"
+              src="https://raw.githubusercontent.com/paulords95/novadax-app/main/Screenshot_20210409-150539_Dax%20API.jpg"
+            />
+            <img
+              alt="3"
+              src="https://raw.githubusercontent.com/paulords95/novadax-app/main/Screenshot_20210409-150547_Dax%20API.jpg"
+            />
+            <img
+              alt="4"
+              src="https://raw.githubusercontent.com/paulords95/novadax-app/main/Screenshot_20210409-150551_Dax%20API.jpg"
+            />
+          </Carousel>
+        </div>
       </div>
       <div className="column-info">
         <h3>Dax App - React Native, Node.js e PostgreSQL</h3>
